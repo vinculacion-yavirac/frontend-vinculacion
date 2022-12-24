817022191854d1e40bd1fbce2c7a2feb0c012720
Steveen Ordoñez <zh311505@gmail.com>
2022-12-24 18:01:57 -0500

    feat:crear pages para docente vinculacion

--- frontend-vinculacion/src/app/app.module.ts
+++ frontend-vinculacion/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AsignarFundacionComponent } from './pages/docente-vinculacion/asignar-fundacion/asignar-fundacion.component';
+import { ConfiguracionComponent } from './pages/docente-vinculacion/configuracion/configuracion.component';
+import { SolicitudComponent } from './pages/docente-vinculacion/solicitud/solicitud.component';
+import { PortafolioComponent } from './pages/docente-vinculacion/portafolio/portafolio.component';
+import { ProyectoComponent } from './pages/docente-vinculacion/proyecto/proyecto.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AsignarFundacionComponent,
+    ConfiguracionComponent,
+    SolicitudComponent,
+    PortafolioComponent,
+    ProyectoComponent,
+    PagesComponent,
+
   ],
   imports: [
     BrowserModule,
