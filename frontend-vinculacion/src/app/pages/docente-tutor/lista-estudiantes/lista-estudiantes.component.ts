f4fdd229be2907a903c36d77bdc46ae4db7ebb9a
grmvera <grm.vera@yavirac.edu.ec>
2022-12-24 19:51:49 -0500

    feat: crear pages docente tutor

---
+++ frontend-vinculacion/src/app/pages/docente-tutor/lista-estudiantes/lista-estudiantes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-estudiantes',
+  templateUrl: './lista-estudiantes.component.html',
+  styleUrls: ['./lista-estudiantes.component.css']
+})
+export class ListaEstudiantesComponent {
+
+}
--- frontend-vinculacion/src/app/pages/pages.module.ts
+++ frontend-vinculacion/src/app/pages/pages.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListaFundacionesComponent } from './docente-tutor/lista-fundaciones/lista-fundaciones.component';
+import { ListaEstudiantesComponent } from './docente-tutor/lista-estudiantes/lista-estudiantes.component';
+import { PortafolioComponent } from './docente-tutor/portafolio/portafolio.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListaFundacionesComponent,
+    ListaEstudiantesComponent,
+    PortafolioComponent
+  ],
   imports: [
     CommonModule
   ]
