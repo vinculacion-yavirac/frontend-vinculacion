bc62032be5648765468f5e2a28571c561c1c5f74
Steveen Ordoñez <zh311505@gmail.com>
2022-12-27 22:56:11 -0500

    fix:portafolio tutor

--- frontend-vinculacion/src/app/app-routing.module.ts
+++ frontend-vinculacion/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages/pages.component';
-import { PortafolioComponent } from './pages/docente-tutor/portafolio/portafolio.component';
 import { SolicitudVinculacionComponent } from './pages/docente-vinculacion/solicitud-vinculacion/solicitud-vinculacion.component';
 import { ProyectoVinculacionComponent } from './pages/docente-vinculacion/proyecto-vinculacion/proyecto-vinculacion.component';
 import { ConfiguracionVinculacionComponent } from './pages/docente-vinculacion/configuracion-vinculacion/configuracion-vinculacion.component';
 import { AsignarFundacionVinculacionComponent } from './pages/docente-vinculacion/asignar-fundacion-vinculacion/asignar-fundacion-vinculacion.component';
+import { PortafolioVinculacionComponent } from './pages/docente-vinculacion/portafolio-vinculacion/portafolio-vinculacion.component';
 
 const routes: Routes = [
   {
@@ -19,7 +19,7 @@ const routes: Routes = [
   { path: 'solicitud', component: SolicitudVinculacionComponent },
   { path: 'asignar-fundacion', component: AsignarFundacionVinculacionComponent },
   { path: 'configuraciones', component: ConfiguracionVinculacionComponent},
-  { path: 'portafolio', component: PortafolioComponent},
+  { path: 'portafolio-vinculacion', component: PortafolioVinculacionComponent},
   { path: 'proyecto', component: ProyectoVinculacionComponent},
   // // {path:'', redirectTo:'/dashboard', pathMatch:'full'},
   // { path: '**', component: NopagesfoundComponent },
--- frontend-vinculacion/src/app/pages/pages.module.ts
+++ frontend-vinculacion/src/app/pages/pages.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaFundacionesComponent } from './docente-tutor/lista-fundaciones/lista-fundaciones.component';
 import { ListaEstudiantesComponent } from './docente-tutor/lista-estudiantes/lista-estudiantes.component';
-import { PortafolioComponent } from './docente-tutor/portafolio/portafolio.component';
 import {HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { PortafolioVinculacionComponent } from './docente-vinculacion/portafolio-vinculacion/portafolio-vinculacion.component';
@@ -10,24 +9,25 @@ import { SolicitudVinculacionComponent } from './docente-vinculacion/solicitud-v
 import { ProyectoVinculacionComponent } from './docente-vinculacion/proyecto-vinculacion/proyecto-vinculacion.component';
 import { ConfiguracionVinculacionComponent } from './docente-vinculacion/configuracion-vinculacion/configuracion-vinculacion.component';
 import { AsignarFundacionVinculacionComponent } from './docente-vinculacion/asignar-fundacion-vinculacion/asignar-fundacion-vinculacion.component';
+import { PortafolioTutorComponent } from './docente-tutor/portafolio-tutor/portafolio-tutor.component';
 
 
 @NgModule({
   declarations: [
     ListaFundacionesComponent,
     ListaEstudiantesComponent,
-    PortafolioComponent,
     AsignarFundacionVinculacionComponent,
-    PortafolioVinculacionComponent,
     SolicitudVinculacionComponent,
+    PortafolioVinculacionComponent,
     ProyectoVinculacionComponent,
     ConfiguracionVinculacionComponent,
+    PortafolioTutorComponent,
   ],
   exports:[
     AsignarFundacionVinculacionComponent,
     ConfiguracionVinculacionComponent,
-    PortafolioComponent,
     PortafolioVinculacionComponent,
+    PortafolioTutorComponent,
     ProyectoVinculacionComponent,
     SolicitudVinculacionComponent
   ],
