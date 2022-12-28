7ae0e2620271c4486db211e678adf57d39788cd4
basganan <bas.ganan@yavirac.edu.ec>
2022-12-28 14:09:00 -0500

    update:solucion de problema module

--- frontend-vinculacion/src/app/app.module.ts
+++ frontend-vinculacion/src/app/app.module.ts
@@ -5,11 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 import {HttpClientModule } from '@angular/common/http';
-import { RedireccionarComponent } from './redireccionar/redireccionar.component';
-import { AgregarRolComponent } from './agregar-rol/agregar-rol.component';
-import { RellenarDatosComponent } from './rellenar-datos/rellenar-datos.component';
-import { ListaEstudiantesComponent } from './lista-estudiantes/lista-estudiantes.component';
-import { ListaPortafoliosComponent } from './lista-portafolios/lista-portafolios.component';
+import { AgregarRolComponent } from './pages/coordinador-carrera/agregar-rol/agregar-rol.component';
+import { ListaEstudiantesComponent } from './pages/coordinador-carrera/lista-estudiantes/lista-estudiantes.component';
+import { ListaPortafoliosComponent } from './pages/coordinador-carrera/lista-portafolios/lista-portafolios.component';
+import { RedireccionarComponent } from './pages/coordinador-carrera/redireccionar/redireccionar.component';
+import { RellenarDatosComponent } from './pages/coordinador-carrera/rellenar-datos/rellenar-datos.component';
 
 
 
