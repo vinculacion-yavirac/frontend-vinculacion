33aff6825b15a91d2fb95ba1629f2767e266640a
Steveen Ordoñez <zh311505@gmail.com>
2022-12-27 22:58:03 -0500

    fix:lista estudiante tutor

--- frontend-vinculacion/src/app/pages/pages.module.ts
+++ frontend-vinculacion/src/app/pages/pages.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaFundacionesComponent } from './docente-tutor/lista-fundaciones/lista-fundaciones.component';
-import { ListaEstudiantesComponent } from './docente-tutor/lista-estudiantes/lista-estudiantes.component';
 import {HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { PortafolioVinculacionComponent } from './docente-vinculacion/portafolio-vinculacion/portafolio-vinculacion.component';
@@ -10,18 +9,19 @@ import { ProyectoVinculacionComponent } from './docente-vinculacion/proyecto-vin
 import { ConfiguracionVinculacionComponent } from './docente-vinculacion/configuracion-vinculacion/configuracion-vinculacion.component';
 import { AsignarFundacionVinculacionComponent } from './docente-vinculacion/asignar-fundacion-vinculacion/asignar-fundacion-vinculacion.component';
 import { PortafolioTutorComponent } from './docente-tutor/portafolio-tutor/portafolio-tutor.component';
+import { ListaEstudiantesTutorComponent } from './docente-tutor/lista-estudiantes-tutor/lista-estudiantes-tutor.component';
 
 
 @NgModule({
   declarations: [
     ListaFundacionesComponent,
-    ListaEstudiantesComponent,
     AsignarFundacionVinculacionComponent,
     SolicitudVinculacionComponent,
     PortafolioVinculacionComponent,
     ProyectoVinculacionComponent,
     ConfiguracionVinculacionComponent,
     PortafolioTutorComponent,
+    ListaEstudiantesTutorComponent,
   ],
   exports:[
     AsignarFundacionVinculacionComponent,
