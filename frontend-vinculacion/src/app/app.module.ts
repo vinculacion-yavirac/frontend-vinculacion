33fb0c7478e8189dbf2e870181f737aeb4ced85d
Steveen Ordoñez <zh311505@gmail.com>
2022-12-27 22:01:07 -0500

    feat:crear routing

--- frontend-vinculacion/src/app/app.module.ts
+++ frontend-vinculacion/src/app/app.module.ts
@@ -3,28 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AsignarFundacionComponent } from './pages/docente-vinculacion/asignar-fundacion/asignar-fundacion.component';
-import { ConfiguracionComponent } from './pages/docente-vinculacion/configuracion/configuracion.component';
-import { SolicitudComponent } from './pages/docente-vinculacion/solicitud/solicitud.component';
-import { PortafolioComponent } from './pages/docente-vinculacion/portafolio/portafolio.component';
-import { ProyectoComponent } from './pages/docente-vinculacion/proyecto/proyecto.component';
-import { PagesComponent } from './pages/pages.component';
+import { PagesModule } from './pages/pages.module';
+import {HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AsignarFundacionComponent,
-    ConfiguracionComponent,
-    SolicitudComponent,
-    PortafolioComponent,
-    ProyectoComponent,
-    PagesComponent,
-
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
