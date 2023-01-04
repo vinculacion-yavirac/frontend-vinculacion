624735668a125cd5fa568e7e4593a45444ac3248
sjvguerra <sjv.guerra@yavirac.edu.ec>
2023-01-04 01:01:36 -0500

    bug: pages estudiante

---
+++ frontend-vinculacion/src/app/pages/estudiante/estudiante-portafolio/estudiante-portafolio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estudiante-portafolio',
+  templateUrl: './estudiante-portafolio.component.html',
+  styleUrls: ['./estudiante-portafolio.component.css']
+})
+export class EstudiantePortafolioComponent {
+
+}
