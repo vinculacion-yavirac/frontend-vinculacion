7f4b3ff56ef852cc676728ddd12f996b3c24f96a
basganan <bas.ganan@yavirac.edu.ec>
2023-01-28 16:50:40 -0500

    update:pantalla agregar rol terminado y arreglado

---
+++ frontend-vinculacion/src/app/pages/coordinador-carrera/agregar-rol/agregar-rol-search/agregar-rol-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agregar-rol-search',
+  templateUrl: './agregar-rol-search.component.html',
+  styleUrls: ['./agregar-rol-search.component.css']
+})
+export class AgregarRolSearchComponent {
+
+}
