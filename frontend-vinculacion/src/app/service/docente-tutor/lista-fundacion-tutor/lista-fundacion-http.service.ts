51e091e69e528b1400fe6d01f3ee0bfccabb6d7c
Rodolfo <grm.vera@yavirac.edu.ec>
2023-01-29 11:40:38 -0500

    feat: servicio de docente tutor lista fundacion

--- frontend-vinculacion/src/app/service/docente-tutor/lista-fundacion-tutor/lista-fundacion-http.service.ts
+++ frontend-vinculacion/src/app/service/docente-tutor/lista-fundacion-tutor/lista-fundacion-http.service.ts
@@ -1,9 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ListaFundacionTutor } from 'src/app/models/docente-tutor/lista-fundacion-tutor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListaFundacionHttpService {
+  constructor(
+    private http: HttpClient
+  ) { }
 
-  constructor() { }
+  private httpOption ={
+    headers: new HttpHeaders({"Content-Type":"application/json"})
+  }
+
+  private url: string = "http://127.0.0.1:8000/api/fundacion/";
+
+  public save(listaFundacionTutor: ListaFundacionTutor): Observable<ListaFundacionTutor>{
+    return this.http.post<ListaFundacionTutor>(this.url+"/save",listaFundacionTutor, this.httpOption);
+  }
+
+  public findById(id: number): Observable<ListaFundacionTutor>{
+    return this.http.get<ListaFundacionTutor>(this.url+"/"+id, this.httpOption);
+  }
+
+  public deleteById(id: number): Observable<ListaFundacionTutor>{
+    return this.http.delete<ListaFundacionTutor>(this.url+"/deleteById/"+id,this.httpOption);
+  }
+
+  public findAll():Observable<ListaFundacionTutor[]>{
+    return this.http.get<ListaFundacionTutor[]>(this.url,this.httpOption);
+  }
+
+  public findByDescription(term: string):Observable<ListaFundacionTutor[]>{
+    return this.http.get<ListaFundacionTutor[]>(this.url+"/findByDescription/"+term,this.httpOption);
+  }
 }
