42407ca2ad84b5f6cd998589b2ac0d9f976f0f0e
Rodolfo <grm.vera@yavirac.edu.ec>
2023-01-29 17:53:07 -0500

    feat:crear register

--- frontend-vinculacion/src/app/app.module.ts
+++ frontend-vinculacion/src/app/app.module.ts
@@ -4,13 +4,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { LoginComponent } from './auth/login/login.component';
-import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    
   ],
   imports: [
     BrowserModule,
