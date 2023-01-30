510a1b2a071b1712aa7f2e89e17732a40221c541
¨Jennifer <jpr.simba@yavirac.edu.ec>
2023-01-30 11:25:45 -0500

    feat:crear pantalla  de informe control

--- frontend-vinculacion/src/app/pages/informe-control/avance-cumplimiento/avance-cumplimiento.component.ts
+++ frontend-vinculacion/src/app/pages/informe-control/avance-cumplimiento/avance-cumplimiento.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AvanceCumplimiento } from 'src/app/models/informe-control/avance-cumplimiento';
+import { AvanceCumplimientoHttpService } from 'src/app/service/informe-control/avance-cumplimiento/avance-cumplimiento-http.service';
 
 @Component({
   selector: 'app-avance-cumplimiento',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./avance-cumplimiento.component.css']
 })
 export class AvanceCumplimientoComponent {
+  constructor(
+    private avanceCumplimientoHttpService:AvanceCumplimientoHttpService
+  ) { }
+
+  avanceC: AvanceCumplimiento[]=[];
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  public findAll(): void{
+    this.avanceCumplimientoHttpService.findAll().subscribe(
+      (response) => this.avanceC = response
+    );
+  }
+
+  public findByDescription(term:string): void{
+    if(term.length >= 2){
+      this.avanceCumplimientoHttpService.findByDescription(term).subscribe(
+        (response) => this.avanceC = response
+      )
+    }
+    if(term.length == 0){
+      this.findAll();
+    }
+  }
+
 
 }
