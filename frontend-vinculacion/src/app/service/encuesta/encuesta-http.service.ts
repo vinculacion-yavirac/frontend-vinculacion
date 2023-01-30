15b42b21763ee9ab50a36291187798e9f7a66248
SANTIAGO DAVID GUALLICHICO QUIISHPE <sdq.guallichico@yavirac.edu.ec>
2023-01-30 13:20:27 -0500

    update: actualizacion service-encuesta

--- frontend-vinculacion/src/app/service/encuesta/encuesta-http.service.ts
+++ frontend-vinculacion/src/app/service/encuesta/encuesta-http.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { CreateRespuestaDto, RespuestaModel, UpdateRespuestaDto } from 'src/app/models/encuesta/respuesta.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EncuestaHttpService {
+  readonly API_URL = 'http://localhost:4200/encuesta';
+  constructor(private httpClient: HttpClient) {
 
-  constructor() { }
+   }
+
+   getAll():Observable<RespuestaModel[]> {
+    const url = `${this.API_URL}`;
+    return this.httpClient.get<RespuestaModel[]>(url);
+  }
+
+  create(product: CreateRespuestaDto):Observable<RespuestaModel> {
+    const url = `${this.API_URL}`;
+    return this.httpClient.post<RespuestaModel>(url, product);
+  }
+  update(id:RespuestaModel['id'], product: UpdateRespuestaDto):Observable<RespuestaModel> {
+    const url = `${this.API_URL}/${id}`;
+    return this.httpClient.put<RespuestaModel>(url, product);
+  }
+  destroy(id:RespuestaModel['id']):Observable<any> {
+    const url = `${this.API_URL}/${id}`;
+    return this.httpClient.delete<any>(url).
+    pipe(map((response:{rta:boolean})=>{return response.rta;})
+    );
+  }
+
+
 }
