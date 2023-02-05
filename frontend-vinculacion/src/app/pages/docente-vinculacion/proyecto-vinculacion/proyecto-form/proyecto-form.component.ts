df12a38c0c997ca500f4a78ea54e06e7db042f5f
hscordonez1810 <hsc.ordonez@yavirac.edu.ec>
2023-02-05 14:46:51 -0500

    feat:proyecto service y models

--- frontend-vinculacion/src/app/pages/docente-vinculacion/proyecto-vinculacion/proyecto-form/proyecto-form.component.ts
+++ frontend-vinculacion/src/app/pages/docente-vinculacion/proyecto-vinculacion/proyecto-form/proyecto-form.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ProyectoVinculacion } from 'src/app/models/docente-vinculacion/proyecto-vinculacion';
+import { PortafolioVinculacionHttpService } from 'src/app/service/docente-vinculacion/portafolio-vinculacion/portafolio-vinculacion-http.service';
 
 @Component({
   selector: 'app-proyecto-form',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class ProyectoFormComponent {
 
+  @Output() termEmitter = new EventEmitter<ProyectoVinculacion>();
+  constructor(
+    private portafolioVinculacionHttpService:PortafolioVinculacionHttpService
+  ) { }
+ 
+  pages: number = 1;
+  solicitudList: ProyectoVinculacion[]=[];
+ 
+  ngOnInit(): void {
+    this.findAll();
+  }
+ 
+  public findAll(): void{
+    this.portafolioVinculacionHttpService.findAll().subscribe(
+      (response) => this.solicitudList = response);
+  }
+ 
+  public onInput(term: string) {
+    if (term.length >= 1) {
+      this.portafolioVinculacionHttpService.findByNombre(term).subscribe(
+        (response) => this.solicitudList = response
+      )
+    }
+    if (term.length === 0) {
+      this.findAll()
+    }
+  }
 }
--- frontend-vinculacion/src/app/service/docente-vinculacion/portafolio-vinculacion/portafolio-vinculacion-http.service.ts
+++ frontend-vinculacion/src/app/service/docente-vinculacion/portafolio-vinculacion/portafolio-vinculacion-http.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProyectoVinculacion } from 'src/app/models/docente-vinculacion/proyecto-vinculacion';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PortafolioVinculacionHttpService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private httpOption ={
+    headers: new HttpHeaders({"Content-Type":"application/json"})
+  }
+
+  private url: string = "http://localhost:8080/api/proyecto";
+
+  public save(proyectoVinculacion: ProyectoVinculacion): Observable<ProyectoVinculacion>{
+    return this.http.post<ProyectoVinculacion>(this.url+"/save",proyectoVinculacion, this.httpOption);
+  }
+
+  public findById(id: number): Observable<ProyectoVinculacion>{
+    return this.http.get<ProyectoVinculacion>(this.url+"/"+id, this.httpOption);
+  }
+
+  public deleteById(id: number): Observable<ProyectoVinculacion>{
+    return this.http.delete<ProyectoVinculacion>(this.url+"/deleteById/"+id,this.httpOption);
+  }
+
+  public findAll():Observable<ProyectoVinculacion[]>{
+    return this.http.get<ProyectoVinculacion[]>(this.url+"/findAll",this.httpOption);
+  }
+
+  public findByNombre(term: string):Observable<ProyectoVinculacion[]>{
+    return this.http.get<ProyectoVinculacion[]>(this.url+"/findByNombre/"+term,this.httpOption);
+  }
 }
