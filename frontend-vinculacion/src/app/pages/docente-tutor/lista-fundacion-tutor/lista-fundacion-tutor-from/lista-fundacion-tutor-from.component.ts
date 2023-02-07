4d424ae41a4b1b1c8035447aa91e145171362495
Rodolfo <grm.vera@yavirac.edu.ec>
2023-02-07 11:28:10 -0500

    update: lista estudiante

--- frontend-vinculacion/src/app/pages/docente-tutor/lista-fundacion-tutor/lista-fundacion-tutor-from/lista-fundacion-tutor-from.component.ts
+++ frontend-vinculacion/src/app/pages/docente-tutor/lista-fundacion-tutor/lista-fundacion-tutor-from/lista-fundacion-tutor-from.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Persona } from 'src/app/models/coordinador-carrera/person';
 import { ListaFundacionTutor } from 'src/app/models/docente-tutor/lista-fundacion-tutor';
 import { VistaFundacionEstudiante } from 'src/app/models/vista/vistaFundacionEstudiante';
 import { ListaFundacionHttpService } from 'src/app/service/docente-tutor/lista-fundacion-tutor/lista-fundacion-http.service';
@@ -10,33 +11,32 @@ import { VistaFundacionEstudianteHttpService } from 'src/app/service/vista/vista
   styleUrls: ['./lista-fundacion-tutor-from.component.css']
 })
 export class ListaFundacionTutorFromComponent {
+     // @Input () prueba: Catalogo;
+     @Output() termEmitter = new EventEmitter<Persona>();
+     constructor(
+       private vistaFundacionEstudianteHttpService:VistaFundacionEstudianteHttpService,
+     ) { }
 
-  // @Input () prueba: Catalogo;
- @Output() termEmitter = new EventEmitter<ListaFundacionTutor>();
- constructor(
-   private vistaFundacionEstudianteHttpService:VistaFundacionEstudianteHttpService,
- ) { }
+     pages: number = 1;
+     vistaFundacionEstudiante: VistaFundacionEstudiante []=[];
 
- pages: number = 1;
- vistaFundacionEstudiante: VistaFundacionEstudiante []=[];
+     ngOnInit(): void {
+       this.findAll();
+     }
 
- ngOnInit(): void {
-   this.findAll();
- }
+    public findAll(): void{
+      this.vistaFundacionEstudianteHttpService.findAll().subscribe(
+        (response) => this.vistaFundacionEstudiante = response );
+    }
 
-public findAll(): void{
-  this.vistaFundacionEstudianteHttpService.findAll().subscribe(
-    (response) => this.vistaFundacionEstudiante = response );
-}
-
- public onInput(term: string) {
-   if (term.length >= 1) {
-     this.vistaFundacionEstudianteHttpService.findByTipoSolicitud(term).subscribe(
-       (response) => this.vistaFundacionEstudiante = response
-     )
-   }
-   if (term.length === 0) {
-     this.findAll()
-   }
- }
+     public onInput(term: string) {
+       if (term.length >= 1) {
+         this.vistaFundacionEstudianteHttpService.findByTipoSolicitud(term).subscribe(
+           (response) => this.vistaFundacionEstudiante = response
+         )
+       }
+       if (term.length === 0) {
+         this.findAll()
+       }
+     }
 }
